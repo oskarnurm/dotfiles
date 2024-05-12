e3fe2bdee7d0f29e68eea91e1b0250b7eb3feb8f
oskarnurm <oskar.nurm@gmail.com>
2024-05-12 21:31:41 +0300

    change theme to gruvbox

--- wezterm.lua
+++ wezterm.lua
@@ -4,12 +4,16 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 -- Will provide clearer error messages.
 local config = wezterm.config_builder()
+local act = wezterm.action
+
+-- Spawn a fish shell in login mode
+config.default_prog = { "/usr/local/bin/fish", "-l" }
 
 -- Colors
-config.color_scheme = "github_dark_high_contrast"
+config.color_scheme = "Gruvbox dark, hard (base16)"
 
 -- Fonts
-config.font = wezterm.font("Hack Nerd Font Mono", { weight = "Bold", italic = false })
+config.font = wezterm.font("JetBrains Mono", { weight = "Regular", italic = false })
 config.font_size = 14.0
 
 -- Tabs
@@ -23,8 +27,55 @@ config.window_frame = {
 	inactive_titlebar_bg = "#000000",
 }
 
+-- Padding
 config.window_padding = {
 	left = 15,
+}
+
+-- Keys
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	-- Send C-a when pressing C-a twice
+	{ key = "a", mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+	{ key = "c", mods = "LEADER", action = act.ActivateCopyMode },
+	{ key = "phys:Space", mods = "LEADER", action = act.ActivateCommandPalette },
+
+	-- Pane keybindings
+	{ key = "_", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "|", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+	{ key = "q", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+	{ key = "o", mods = "LEADER", action = act.RotatePanes("Clockwise") },
+	-- We can make separate keybindings for resizing panes
+	-- But Wezterm offers custom "mode" in the name of "KeyTable"
+	{
+		key = "r",
+		mods = "LEADER",
+		action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
+	},
+}
+
+config.key_tables = {
+	resize_pane = {
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
+	move_tab = {
+		{ key = "h", action = act.MoveTabRelative(-1) },
+		{ key = "j", action = act.MoveTabRelative(-1) },
+		{ key = "k", action = act.MoveTabRelative(1) },
+		{ key = "l", action = act.MoveTabRelative(1) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
 }
 
 -- and finally, return the configuration to wezterm
