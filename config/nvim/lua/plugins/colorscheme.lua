10260419be49b38d8f3d36e82759ed32c7d38238
oskarnurm <oskar.nurm@gmail.com>
2024-05-12 21:29:49 +0300

    change colorscheme to gruvbox

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,26 @@
 return {
-  "projekt0n/github-nvim-theme",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("github-theme").setup {
-      options = {
-        transparent = true, -- Disable setting background
-      },
-    }
-
-    vim.cmd "colorscheme github_dark_high_contrast"
-  end,
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("gruvbox").setup {
+        transparent_mode = true,
+      }
+      vim.cmd "colorscheme gruvbox"
+    end,
+  },
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("github-theme").setup {
+  --       options = {
+  --         transparent = true, -- Disable setting background
+  --       },
+  --     }
+  --     -- vim.cmd "colorscheme github_dark_high_contrast"
+  --   end,
+  -- },
 }
