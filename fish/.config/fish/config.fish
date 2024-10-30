2f11d8287c2b78411c78147ada6cc58d4b930956
oskarnurm <19738295+oskarnurm@users.noreply.github.com.>
2024-10-30 17:10:48 +0100

    change git abreviatons

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,7 +14,7 @@ alias lt='eza --icons=auto --tree' # list folder as tree
 # Handy change dir shortcuts
 abbr .. 'cd ..'
 abbr ... 'cd ../..'
-abbr gc 'git commit -m"'
+abbr gc 'git commit -m'
 abbr ga 'git add'
 abbr gt 'git status'
 
