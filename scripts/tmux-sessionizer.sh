2c2fa871c7f0f54be026cbf7f2ae7c91c449a5a3
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-01-07 15:18:55 +0200

    Update script
    Before the script was more complicated and was not working properly.
    Changed it back to a much more simplistic version with only selecting
    directories and creating sessions. Also added a prettier preview.

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -1,29 +1,14 @@
 #!/usr/bin/env bash
 
+settings="if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi"
+
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-selected=$(
-  find ~/dotfiles/config ~/dotfiles ~/programming \
-    -mindepth 1 -maxdepth 4 \
-    \( -path '*/node_modules' -prune \) -o -print \
-  | fzf --preview '
-    if [ -d {} ]; then
-      eza --tree --color=always {}
-    elif [ -f {} ]; then
-      bat --style=numbers --color=always --line-range :500 {}
-    fi
-  '
-)
+    selected=$(find ~/dotfiles ~/projects ~/learning -mindepth 1 -maxdepth 1 -type d | fzf --preview "$settings")
 fi
 
 if [[ -z $selected ]]; then
-    exit 0
-fi
-
-if [ -f "$selected" ]; then
-    # If the selected item is a file, open it in neovim
-    nvim "$selected"
     exit 0
 fi
 
@@ -31,29 +16,12 @@ selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    # No tmux session running, start a new session with a new window
-    tmux new-session -s $selected_name -n $selected_name -c $selected
+    tmux new-session -s $selected_name -c $selected
     exit 0
 fi
 
-# Check if already inside a tmux session or not
-if [[ -n $TMUX ]]; then
-    # Create a new window in the existing session
-    if ! tmux list-windows | grep -q $selected_name; then
-        tmux new-window -n $selected_name -c $selected
-    fi
-else
-    # Not inside a tmux session, attach to existing or create a new one
-    if ! tmux has-session -t $selected_name 2> /dev/null; then
-        tmux new-session -ds $selected_name
-        tmux new-window -t $selected_name -n $selected_name -c $selected
-    else
-        if ! tmux list-windows -t $selected_name | grep -q $selected_name; then
-            tmux new-window -t $selected_name -n $selected_name -c $selected
-        fi
-    fi
-    tmux attach-session -t $selected_name
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
 fi
 
-# Switch to the window with the selected name
-tmux select-window -t $selected_name
+tmux switch-client -t $selected_name
