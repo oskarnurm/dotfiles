0f6c64db1053c154e204c81b201fa0c08c08ea63
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-04-15 14:07:22 +0200

    chore: Move Monochrome colorscheme to colors/
    Previously stored inside the nvim/ folder, moved away as I  would like
    to keep editing it someday/maybe

---
+++ colors/monochrome.lua
@@ -0,0 +1,8 @@
+-- Monochrome colorscheme edit based on quiet
+vim.cmd("colorscheme quiet")
+vim.api.nvim_set_hl(0, "Keyword", { fg = "#FFFFFF", bold = true })
+vim.api.nvim_set_hl(0, "Comment", { fg = "#888888", italic = true })
+vim.api.nvim_set_hl(0, "Constant", { fg = "#999999" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000" })
+vim.api.nvim_set_hl(0, "Visual", { bg = "#71797E", fg = "NONE" })
