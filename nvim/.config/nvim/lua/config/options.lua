42651f0d1d8bc8abbede7e6c935c133da451289a
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-04-27 22:50:13 +0200

    feat: enable neovims default lsp completion
    I don't use completions often, but when I do, then I prefer the ones set by lsp

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -38,6 +38,7 @@ vim.opt.undofile = true
 vim.opt.undodir = os.getenv "HOME" .. "/.neovim/undodir"
 vim.opt.splitright = true
 
+-- fix paths for Snacks.picker that we otherwise can't see
 vim.api.nvim_set_hl(0, "SnacksPickerDir", { fg = "#676767" })
 vim.api.nvim_set_hl(0, "SnacksPickerPathHidden", { fg = "#676767" })
 
@@ -47,5 +48,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.hl.on_yank()
+  end,
+})
+
+-- lsp-completion
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp-completion", { clear = true }),
+  callback = function(event)
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+    if client and client:supports_method "textDocument/completion" then
+      vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = false })
+    end
   end,
 })
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,14 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    event = { "BufRead", "BufNewFile" },
     dependencies = {
       { "williamboman/mason.nvim", config = true },
     },
-    -- We can lazy load the plugin to save a few milliseconds of startup time,
-    -- we just need the nvim-lspconfig in the runtime path
-    lazy = true,
-    init = function()
-      local lspcconfig_path = require("lazy.core.config").options.root .. "/nvim-lspconfig"
-      vim.opt.runtimepath:append(lspcconfig_path)
+    -- lazy = true,
+    config = function()
+      -- local lspcconfig_path = require("lazy.core.config").options.root .. "/nvim-lspconfig"
+      -- vim.opt.runtimepath:append(lspcconfig_path)
 
       vim.lsp.enable { "lua_ls", "ts_ls", "vtsls", "pyright", "clangd", "jdtls" }
 
