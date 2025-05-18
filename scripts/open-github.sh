cf231cacbca54df26b51edacd53efe107dd184c9
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-05-18 13:27:43 +0200

    chore: update open-github.sh script
    Just use gh

--- scripts/open-github.sh
+++ scripts/open-github.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Open the current repository in the browser
+# NOTE: Just use 'gh' if you have it installed
 
 # Goto the root of the repo for the script to work
 dir=$(tmux run "echo #{pane_start_path}")
@@ -9,10 +10,15 @@ url=$(git remote get-url origin)
 # Check if the repository is on GitHub
 if [[ $url =~ ^git@github\.com:(.+)\.git$ ]]; then
   url="https://github.com/${BASH_REMATCH[1]}"
-  open $url
 elif [[ $url =~ ^https://github\.com/.+\.git$ ]]; then
   url="${url%.git}"
-  open $url
 else
-  echo "This repository is not hosted on GitHub"
+  echo "Not a Github repo"
+  exit 1
+fi
+
+if command -v open &>/dev/null; then
+  open "$url"
+else
+  echo "Could not open with 'open'"
 fi
