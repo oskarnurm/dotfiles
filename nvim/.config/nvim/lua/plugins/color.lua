e2752531e4c56f06ce5b64228fd5adb4ffdb396f
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-05-31 00:52:47 +0200

    chore: add vague to colorschemes

--- nvim/.config/nvim/lua/plugins/color.lua
+++ nvim/.config/nvim/lua/plugins/color.lua
@@ -1,8 +1,10 @@
 return {
-  "vague2k/vague.nvim",
-  config = function()
-    require("vague").setup { transparent = true }
-    vim.cmd "colorscheme vague"
-    vim.cmd ":hi statusline guibg=NONE"
-  end,
+  {
+    "vague2k/vague.nvim",
+    config = function()
+      require("vague").setup { transparent = true }
+      vim.cmd "colorscheme vague"
+      vim.cmd ":hi statusline guibg=NONE"
+    end,
+  },
 }
