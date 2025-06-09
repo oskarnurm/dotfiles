e08ae885a739f57ad787397766a2ee2eebcea2a0
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-06-09 21:17:33 +0200

    chore: turn of send_compose_key_when_left_alt_is_pressed

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,6 +4,10 @@ local config = wezterm.config_builder()
 -- config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
 config.max_fps = 120
 
+-- Disable Option key composition
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = false
+
 config.window_background_opacity = 1.0
 
 config.colors = {
