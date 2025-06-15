df779efd8bbdd69670aa905e2b19975b93fc5793
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-06-15 19:17:59 +0200

    chore: fix formating

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -6,8 +6,13 @@ return {
   opts = {},
   keys = function()
     local keys = {
+      { "<leader>h", function()
+          local harpoon = require "harpoon"
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Harpoon Quick Menu",
+      },
       { "<leader>H", function() require("harpoon"):list():add() end, desc = "Harpoon File", },
-      { "<leader>h", function() require("harpoon").ui:toggle_quick_menu(harpoon:list()) end, desc = "Harpoon Quick Menu", },
       { "<leader>j", function() require("harpoon"):list():select(1) end, desc = "Harpoon to File 1", },
       { "<leader>k", function() require("harpoon"):list():select(2) end, desc = "Harpoon to File 2", },
       { "<leader>l", function() require("harpoon"):list():select(3) end, desc = "Harpoon to File 3", },
