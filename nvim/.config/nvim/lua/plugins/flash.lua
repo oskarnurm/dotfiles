60ad28aa051bffc379ff55c6447e59558f9d29cd
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-07-18 00:00:03 +0300

    feat: add flash

---
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      search = {
+        enabled = true,
+      },
+    },
+  },
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+  },
+}
