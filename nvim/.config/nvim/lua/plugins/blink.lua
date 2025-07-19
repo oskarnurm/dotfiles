fa11069cc1eb41f8f4872c2039234988eb00b239
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-07-19 20:30:50 +0300

    feat: add lazydev to blink source

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,19 +1,26 @@
 return {
-  {
-    enabled = true,
-    "saghen/blink.cmp",
-    dependencies = { "rafamadriz/friendly-snippets" },
+  "saghen/blink.cmp",
+  event = "VimEnter",
+  version = "1.*",
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    "folke/lazydev.nvim",
+  },
+  opts = {
+    keymap = { preset = "default" },
 
-    version = "1.*",
-    opts = {
-      keymap = { preset = "default" },
-      appearance = {
-        nerd_font_variant = "mono",
-      },
-      completion = {
-        menu = { auto_show = true },
-        documentation = { auto_show = false },
+    appearance = { nerd_font_variant = "mono" },
+    completion = {
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "lazydev", "buffer" },
+      providers = {
+        lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
       },
     },
+    fuzzy = { implementation = "prefer_rust_with_warning" },
+
+    signature = { enabled = true },
   },
 }
