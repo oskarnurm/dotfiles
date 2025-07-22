66bf5c8b270eac436cd60887b0c3c6c3045075e5
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-07-22 14:10:02 +0300

    feat: add gitsigns related keymaps

--- nvim/.config/nvim/lua/plugins/gitsings.lua
+++ nvim/.config/nvim/lua/plugins/gitsings.lua
@@ -8,5 +8,25 @@ return {
       topdelete = { text = "‾" },
       changedelete = { text = "~" },
     },
+    on_attach = function(bufnr)
+      local gitsigns = require "gitsigns"
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- stylua: ignore start
+        map("v", "<leader>hs", function() gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" } end, { desc = "Stage hunk" })
+        map("v", "<leader>hr", function() gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" } end, { desc = "Reset hunk" })
+        map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage" })
+        map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset" })
+        map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "Stage buffer" })
+        map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Reset buffer" })
+        map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview" })
+        map("n", "<leader>hb", gitsigns.blame_line, { desc = "Blame line" })
+        map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Blame line" })
+    end,
   },
 }
--- nvim/.config/nvim/lua/plugins/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/vim-fugitive.lua
@@ -2,10 +2,10 @@ return {
   "tpope/vim-fugitive",
   event = "VeryLazy",
   init = function()
-    vim.keymap.set("n", "<leader>hs", "<cmd>Git<CR>", { desc = "Git status" })
-    vim.keymap.set("n", "<leader>hw", "<cmd>Gwrite<CR>", { desc = "Stage current file" })
+    -- vim.keymap.set("n", "<leader>hs", "<cmd>Git<CR>", { desc = "Git status" })
+    -- vim.keymap.set("n", "<leader>hw", "<cmd>Gwrite<CR>", { desc = "Stage current file" })
 
-    vim.keymap.set("n", "<leader>hd", "<cmd>Gdiffsplit<CR>", { desc = "Git diff" })
+    vim.keymap.set("n", "<leader>ho", "<cmd>Gvdiffsplit<CR>", { desc = "Open vimdiff" })
     vim.keymap.set("n", "<leader>he", "<cmd>only<CR>", { desc = "Exit vimdiff" })
 
     vim.keymap.set("n", "<leader>hl", "<cmd>diffget //2<CR>", { desc = "Get left (Merge conflict)" })
