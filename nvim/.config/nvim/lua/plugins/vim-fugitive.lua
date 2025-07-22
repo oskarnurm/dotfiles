c431fe030eab0928815f732e88a97b1a710e8bcc
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-07-22 15:15:34 +0300

    feat: fix keymaps and remove unused
    I will probably invoke vim-fugitive with commands

--- nvim/.config/nvim/lua/plugins/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/vim-fugitive.lua
@@ -1,14 +1,9 @@
 return {
   "tpope/vim-fugitive",
   event = "VeryLazy",
-  init = function()
-    vim.keymap.set("n", "<leader>hgs", "<cmd>Git<CR>", { desc = "Status" })
-    vim.keymap.set("n", "<leader>hgw", "<cmd>Gwrite<CR>", { desc = "Stage current file" })
-
-    vim.keymap.set("n", "<leader>hgo", "<cmd>Gvdiffsplit<CR>", { desc = "Open diffsplit" })
-    vim.keymap.set("n", "<leader>hge", "<cmd>only<CR>", { desc = "Exit diffsplit" })
-
-    vim.keymap.set("n", "<leader>hgl", "<cmd>diffget //2<CR>", { desc = "Get left (Merge conflict)" })
-    vim.keymap.set("n", "<leader>hgr", ":<cmd>diffget //3<CR>", { desc = "Get right (Merge conflict)" })
+  config = function()
+    vim.keymap.set("n", "<leader>hg", "<cmd>Gvdiffsplit!<CR>", { desc = "Open Diffsplit" })
+    vim.keymap.set("n", "<leader>hl", "<cmd>diffget //2<CR>", { desc = "Get Diff From 2" })
+    vim.keymap.set("n", "<leader>hr", "<cmd>diffget //3<CR>", { desc = "Get Diff From 3" })
   end,
 }
