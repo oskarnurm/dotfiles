817ddef0403a9db29d61612fb0875cf8840ab45f
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-07-22 14:10:49 +0300

    refactor: change vim-fugitive keymaps so they won't conflict with gitsigns

--- nvim/.config/nvim/lua/plugins/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/vim-fugitive.lua
@@ -2,13 +2,13 @@ return {
   "tpope/vim-fugitive",
   event = "VeryLazy",
   init = function()
-    -- vim.keymap.set("n", "<leader>hs", "<cmd>Git<CR>", { desc = "Git status" })
-    -- vim.keymap.set("n", "<leader>hw", "<cmd>Gwrite<CR>", { desc = "Stage current file" })
+    vim.keymap.set("n", "<leader>hgs", "<cmd>Git<CR>", { desc = "Status" })
+    vim.keymap.set("n", "<leader>hgw", "<cmd>Gwrite<CR>", { desc = "Stage current file" })
 
-    vim.keymap.set("n", "<leader>ho", "<cmd>Gvdiffsplit<CR>", { desc = "Open vimdiff" })
-    vim.keymap.set("n", "<leader>he", "<cmd>only<CR>", { desc = "Exit vimdiff" })
+    vim.keymap.set("n", "<leader>hgo", "<cmd>Gvdiffsplit<CR>", { desc = "Open diffsplit" })
+    vim.keymap.set("n", "<leader>hge", "<cmd>only<CR>", { desc = "Exit diffsplit" })
 
-    vim.keymap.set("n", "<leader>hl", "<cmd>diffget //2<CR>", { desc = "Get left (Merge conflict)" })
-    vim.keymap.set("n", "<leader>hr", ":<cmd>diffget //3<CR>", { desc = "Get right (Merge conflict)" })
+    vim.keymap.set("n", "<leader>hgl", "<cmd>diffget //2<CR>", { desc = "Get left (Merge conflict)" })
+    vim.keymap.set("n", "<leader>hgr", ":<cmd>diffget //3<CR>", { desc = "Get right (Merge conflict)" })
   end,
 }
