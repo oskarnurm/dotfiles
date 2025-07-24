0dcd9c94d1cf0ace4228ef8a57f7f5e8e330c6f0
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-07-24 15:42:38 +0300

    chore: add todo for install.sh
    I am hoping seeing this empty file will trigger me in the future on
    actually working in a proper install setup

---
+++ install.sh
@@ -0,0 +1,6 @@
+# TODO:
+# use curl?
+# use stow?
+# install using brewfile
+# hard link files like mouseless `ln ~/dotfiles/mouseless/config.yaml "$HOME/Library/Containers/net.sonuscape.mouseless/Data/.mouseless/configs/config.yaml"
+`
