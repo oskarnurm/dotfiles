63966ba0e529039a1405c98296103f56da550e4a
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-07-24 14:28:44 +0300

    feat(treesitter): add incremental selection keymaps, textobjects and disable treesitter for large files
    - I was missing some textobjects for functions
    - Incremental selection seems nice, might remove later
    - Turning off treesitter for large files makes sense

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
   -- event = { "BufReadPre" },
   build = ":TSUpdate",
   main = "nvim-treesitter.configs",
@@ -9,11 +12,49 @@ return {
     auto_install = true,
     highlight = {
       enable = true,
+      -- Disable slow treesitter highlight for large files
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
       --  If you are experiencing weird indenting issues, add the language to
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
       additional_vim_regex_highlighting = { "ruby" },
     },
     indent = { enable = true, disable = { "ruby" } },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "zi",
+        node_incremental = "zn",
+        scope_incremental = "zo",
+        node_decremental = "zd",
+      },
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        -- So you we do `vif` even if your cursor is on the `f
+        lookahead = true,
+
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+          ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+        },
+        selection_modes = {
+          ["@parameter.outer"] = "v", -- charwise
+          ["@function.outer"] = "V", -- linewise
+          ["@class.outer"] = "<c-v>", -- blockwise
+        },
+        include_surrounding_whitespace = true,
+      },
+    },
   },
 }
