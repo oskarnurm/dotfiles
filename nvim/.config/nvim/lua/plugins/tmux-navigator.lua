ae7bc851f5c9b3367ba56df7d292f580db7fb269
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-07-25 20:19:02 +0300

    refactor(tmux-navigator): remove unused VeryLazy and cmd events
    We lazy load the plugin with keys anyway, no need for separate event
    loading

--- nvim/.config/nvim/lua/plugins/tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,13 +1,5 @@
 return {
   "christoomey/vim-tmux-navigator",
-  event = "VeryLazy",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-  },
   keys = {
     { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
     { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
