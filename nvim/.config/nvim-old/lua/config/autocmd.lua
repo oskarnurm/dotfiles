539394cba40979230763294e94cb6ed880515fc1
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-08-16 19:00:29 +0300

    chore(nvim-old): format

--- nvim/.config/nvim-old/lua/config/autocmd.lua
+++ nvim/.config/nvim-old/lua/config/autocmd.lua
@@ -8,10 +8,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Auto create dir when saving a file, in case some intermediate directory does not exist
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+vim.api.nvim_create_autocmd("BufWritePre", {
   group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
   callback = function(event)
-    if event.match:match "^%w%w+:[\\/][\\/]" then
+    if event.match:match("^%w%w+:[\\/][\\/]") then
       return
     end
     local file = vim.uv.fs_realpath(event.match) or event.match
@@ -28,7 +28,6 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = true
   end,
 })
-
 
 -- Go to last loc when opening a buffer
 vim.api.nvim_create_autocmd("BufReadPost", {
