0ce14e70ae7073a3920d54f48099b6f0d1925b41
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-08-18 01:55:27 +0300

    feat(clangd): update config
    Changed config to a more minimal setup because I almost never use c

--- nvim/.config/nvim/lsp/clangd.lua
+++ nvim/.config/nvim/lsp/clangd.lua
@@ -64,41 +64,19 @@ end
 ---@field offsetEncoding? string
 
 return {
-	cmd = { "clangd" },
-	filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
-	root_markers = {
-		".clangd",
-		".clang-tidy",
-		".clang-format",
-		"compile_commands.json",
-		"compile_flags.txt",
-		"configure.ac", -- AutoTools
-		".git",
-	},
-	capabilities = {
-		textDocument = {
-			completion = {
-				editsNearCursor = true,
-			},
-		},
-		offsetEncoding = { "utf-8", "utf-16" },
-	},
-	---@param client vim.lsp.Client
-	---@param init_result ClangdInitializeResult
-	on_init = function(client, init_result)
-		if init_result.offsetEncoding then
-			client.offset_encoding = init_result.offsetEncoding
-		end
-	end,
-	---@param client vim.lsp.Client
-	---@param bufnr integer
-	on_attach = function(client, bufnr)
-		vim.api.nvim_buf_create_user_command(bufnr, "LspClangdSwitchSourceHeader", function()
-			switch_source_header(bufnr, client)
-		end, { desc = "Switch between source/header" })
-
-		vim.api.nvim_buf_create_user_command(bufnr, "LspClangdShowSymbolInfo", function()
-			symbol_info(bufnr, client)
-		end, { desc = "Show symbol info" })
-	end,
+  cmd = {
+    "clangd",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--completion-style=detailed",
+    "--fallback-style=none",
+    "--function-arg-placeholders=false",
+  },
+  filetypes = { "c", "cpp" },
+  root_markers = {
+    ".clangd",
+    "compile_commands.json",
+    "Makefile",
+    ".git",
+  },
 }
