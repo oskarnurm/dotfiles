d78fb8978e91be33eb2a8c7d1c5afffacd67d5da
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-08-18 02:00:48 +0300

    feat(init): remove nvim-lpsconfig

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -98,7 +98,6 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" },
   { src = "https://github.com/mason-org/mason.nvim" },
-  { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/rafamadriz/friendly-snippets" },
   { src = "https://github.com/saghen/blink.cmp", version = "v1.6.0" },
   { src = "https://github.com/echasnovski/mini.pick" },
