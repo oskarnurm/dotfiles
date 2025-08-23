f1eecbd9ad09348dd1103ad13e484291fc909ae6
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-08-23 17:56:43 +0200

    feat(nvim): remove vim-tmux-navigator

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -51,7 +51,6 @@ vim.pack.add({
   { src = "https://github.com/mbbill/undotree" },
   { src = "https://github.com/stevearc/oil.nvim" },
   { src = "https://github.com/stevearc/conform.nvim" },
-  { src = "https://github.com/christoomey/vim-tmux-navigator" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" },
   { src = "https://github.com/mason-org/mason.nvim" },
