c505c3160ad21aee63fea87509ee709b0107f06d
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-09-04 05:38:51 +0200

    chore(autocmds): move autocmds to lua/

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,3 +1,11 @@
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- Wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("wrap_spell", { clear = true }),
@@ -34,6 +42,24 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     local lcount = vim.api.nvim_buf_line_count(buf)
     if mark[1] > 0 and mark[1] <= lcount then
       pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Build commands for plugins
+vim.api.nvim_create_autocmd("PackChanged", {
+  desc = "Handle nvim-treesitter updates",
+  group = vim.api.nvim_create_augroup("nvim-treesitter-pack-changed-update-handler", { clear = true }),
+  callback = function(event)
+    if event.data.kind == "update" and event.data.spec.name == "nvim-treesitter" then
+      vim.notify("nvim-treesitter updated, running TSUpdate...", vim.log.levels.INFO)
+      ---@diagnostic disable-next-line: param-type-mismatch
+      local ok = pcall(vim.cmd, "TSUpdate")
+      if ok then
+        vim.notify("TSUpdate completed successfully!", vim.log.levels.INFO)
+      else
+        vim.notify("TSUpdate command not available yet, skipping", vim.log.levels.WARN)
+      end
     end
   end,
 })
