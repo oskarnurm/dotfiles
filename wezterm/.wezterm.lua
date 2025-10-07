98e23ffcd2bc3212bf40d0b8d3e80ef71a8b0995
oskarnurm <19738295+oskarnurm@users.noreply.github.com.>
2025-10-07 17:42:31 +0200

    chore(wez): import vague as a colorscheme and other changes

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local theme = require("lua/vague")
 
 config.max_fps = 120
 config.font_size = 16.0
@@ -8,8 +9,13 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.send_composed_key_when_left_alt_is_pressed = false -- Disable Option key composition
 config.send_composed_key_when_right_alt_is_pressed = false
-config.window_padding = { left = "2cell" }
 config.line_height = 1.3
-config.color_scheme = "rootloops"
+config.color_scheme = "light"
+-- config.colors = theme.colors()
+config.window_padding = {
+	top = 30,
+	bottom = 30,
+	left = 30,
+}
 
 return config
