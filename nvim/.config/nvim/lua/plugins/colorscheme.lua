ee0241cbc4c1c2a5c72dc8aa39c8257c579359bc
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-10-31 23:39:24 +0100

    change filename to colorscheme and add colors

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,13 @@
 return {
   "nickkadutskyi/jb.nvim",
+  "y9san9/y9nika.nvim",
+  "vague-theme/vague.nvim",
+  "p00f/alabaster.nvim",
   priority = 1000,
   lazy = false,
   config = function()
     require("jb").setup({ transparent = true })
-    vim.cmd("colorscheme jb")
+    require("vague").setup({ transparent = true })
     -- vim.cmd(":hi statusline guibg=NONE")
   end,
 }
