62a299352744f695069a1072c5c43cd896fdadcd
oskarnurm <19738295+oskarnurm@users.noreply.github.com.>
2025-12-29 12:44:22 +0200

    feat(nvim): replace pyright by basedpyright
    If I can manage all LSP:s with brew, I should be fine using basedpyright

--- nvim/.config/nvim/lsp/basedpyright.lua
+++ nvim/.config/nvim/lsp/basedpyright.lua
@@ -1,21 +1,21 @@
 ---@brief
 ---
---- https://github.com/microsoft/pyright
+--- https://detachhead.github.io/basedpyright
 ---
---- `pyright`, a static type checker and language server for python
+--- `basedpyright`, a static type checker and language server for python
 ---
---- Install via `npm install -g pyright`
+--- install with `brew install basedpyright`
 
 local function set_python_path(command)
   local path = command.args
   local clients = vim.lsp.get_clients({
     bufnr = vim.api.nvim_get_current_buf(),
-    name = "pyright",
+    name = "basedpyright",
   })
   for _, client in ipairs(clients) do
     if client.settings then
-      client.settings.python =
-        vim.tbl_deep_extend("force", client.settings.python --[[@as table]], { pythonPath = path })
+      ---@diagnostic disable-next-line: param-type-mismatch
+      client.settings.python = vim.tbl_deep_extend("force", client.settings.python or {}, { pythonPath = path })
     else
       client.config.settings = vim.tbl_deep_extend("force", client.config.settings, { python = { pythonPath = path } })
     end
@@ -25,7 +25,7 @@ end
 
 ---@type vim.lsp.Config
 return {
-  cmd = { "pyright-langserver", "--stdio" },
+  cmd = { "basedpyright-langserver", "--stdio" },
   filetypes = { "python" },
   root_markers = {
     "pyrightconfig.json",
@@ -37,7 +37,7 @@ return {
     ".git",
   },
   settings = {
-    python = {
+    basedpyright = {
       analysis = {
         autoSearchPaths = true,
         useLibraryCodeForTypes = true,
@@ -48,11 +48,11 @@ return {
   on_attach = function(client, bufnr)
     vim.api.nvim_buf_create_user_command(bufnr, "LspPyrightOrganizeImports", function()
       local params = {
-        command = "pyright.organizeimports",
+        command = "basedpyright.organizeimports",
         arguments = { vim.uri_from_bufnr(bufnr) },
       }
 
-      -- Using client.request() directly because "pyright.organizeimports" is private
+      -- Using client.request() directly because "basedpyright.organizeimports" is private
       -- (not advertised via capabilities), which client:exec_cmd() refuses to call.
       -- https://github.com/neovim/neovim/blob/c333d64663d3b6e0dd9aa440e433d346af4a3d81/runtime/lua/vim/lsp/client.lua#L1024-L1030
       ---@diagnostic disable-next-line: param-type-mismatch
@@ -60,8 +60,9 @@ return {
     end, {
       desc = "Organize Imports",
     })
+
     vim.api.nvim_buf_create_user_command(bufnr, "LspPyrightSetPythonPath", set_python_path, {
-      desc = "Reconfigure pyright with the provided python path",
+      desc = "Reconfigure basedpyright with the provided python path",
       nargs = 1,
       complete = "file",
     })
