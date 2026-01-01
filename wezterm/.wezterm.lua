c1726e8604c9f9a92d1b64c1fa220ef668f40f9d
oskarnurm <19738295+oskarnurm@users.noreply.github.com.>
2026-01-01 20:15:25 +0200

    feat(wezterm): add escape sequence for Ctrl+Tab and Ctrl+Shift+Tab
    This enables, for example, to switch last windows in tmux using Ctrl+Tab

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,10 +3,10 @@ local config = wezterm.config_builder()
 
 config.max_fps = 120
 config.font_size = 15.0
-config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" } -- Disable font ligatures
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" } -- disable font ligatures
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
-config.send_composed_key_when_left_alt_is_pressed = false -- Disable Option key composition
+config.send_composed_key_when_left_alt_is_pressed = false -- disable Option key composition
 config.send_composed_key_when_right_alt_is_pressed = false
 config.line_height = 1.1
 config.color_scheme = "dark"
@@ -14,6 +14,18 @@ config.window_padding = {
 	top = 30,
 	bottom = 30,
 	left = 30,
+}
+config.keys = {
+	{ -- send the escape sequence for Ctrl+Tab
+		key = "Tab",
+		mods = "CTRL",
+		action = wezterm.action.SendString("\x1b[27;5;9~"),
+	},
+	{ -- send the sequence for Ctrl+Shift+Tab
+		key = "Tab",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SendString("\x1b[27;6;9~"),
+	},
 }
 
 return config
