d762bf9482969fa611d770f36aa98c4587e798b2
oskarnurm <19738295+oskarnurm@users.noreply.github.com.>
2026-02-19 22:18:51 +0100

    chore: update wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,14 +2,13 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.max_fps = 120
-config.font_size = 15.0
+config.font_size = 14.0
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" } -- disable font ligatures
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.send_composed_key_when_left_alt_is_pressed = false -- disable Option key composition
 config.send_composed_key_when_right_alt_is_pressed = false
 config.line_height = 1.1
-config.color_scheme = "dark"
 config.window_padding = {
 	top = 30,
 	bottom = 30,
@@ -27,5 +26,13 @@ config.keys = {
 		action = wezterm.action.SendString("\x1b[27;6;9~"),
 	},
 }
+
+local get_theme = {
+	Light = "koda-dark", -- can change this to light whenever I want
+	Dark = "koda-dark",
+}
+
+local appearance = wezterm.gui.get_appearance()
+config.colors = require(get_theme[appearance])
 
 return config
