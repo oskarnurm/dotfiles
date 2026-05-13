8bd30abac151ed5fc01f99d3b3f25512ebe9237f
oskarnurm <19738295+oskarnurm@users.noreply.github.com.>
2026-05-13 12:05:36 +0200

    chore(nvim): move mini plugins to their own file

---
+++ nvim/.config/nvim/plugin/40_mini.lua
@@ -0,0 +1,60 @@
+local now, now_if_args, later = Config.now, Config.now_if_args, Config.later
+
+now(function()
+  require("mini.icons").setup({})
+  later(MiniIcons.tweak_lsp_kind) -- Add LSP kind icons. Useful for 'mini.completion'.
+end)
+
+-- Example usage:
+-- - `ci)` - *c*hange *i*inside parenthesis (`)`)
+-- - `di(` - *d*elete *i*inside padded parenthesis (`(`)
+-- - `yaq` - *y*ank *a*round *q*uote (any of "", '', or ``)
+-- - `vif` - *v*isually select *i*inside *f*unction call
+-- - `cina` - *c*hange *i*nside *n*ext *a*rgument
+later(function()
+  local ai = require("mini.ai")
+  ai.setup({
+    custom_textobjects = {
+      f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+      c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
+      o = ai.gen_spec.treesitter({
+        a = { "@conditional.outer", "@loop.outer" },
+        i = { "@conditional.inner", "@loop.inner" },
+      }),
+    },
+  })
+end)
+
+-- Example usage:
+-- - `saiw)` - *s*urround *a*dd for *i*nside *w*ord parenthesis (`)`)
+-- - `sdf`   - *s*urround *d*elete *f*unction call (like `f(var)` -> `var`)
+-- - `srb[`  - *s*urround *r*eplace *b*racket (any of [], (), {}) with padded `[`
+-- - `sf*`   - *s*urround *f*ind right part of `*` pair (like bold in markdown)
+-- - `shf`   - *s*urround *h*ighlight current *f*unction call
+-- - `srn{{` - *s*urround *r*eplace *n*ext curly bracket `{` with padded `{`
+-- - `sdl'`  - *s*urround *d*elete *l*ast quote pair (`'`)
+later(function() require("mini.surround").setup() end)
+
+later(function() require("mini.extra").setup() end)
+later(function() require("mini.misc").setup() end)
+later(function() require("mini.jump2d").setup() end)
+
+later(function()
+  require("mini.pick").setup()
+  vim.keymap.set("n", "<leader>f", "<cmd>Pick files<CR>", { desc = "Pick Files" })
+  vim.keymap.set("n", "<leader>m", "<cmd>Pick help<CR>", { desc = "Pick Manual" })
+  vim.keymap.set("n", "<leader>/", "<cmd>Pick buf_lines<CR>", { desc = "Pick Lines" })
+  vim.keymap.set("n", "<leader>g", "<cmd>Pick grep_live<CR>", { desc = "Pick Grep" })
+  vim.keymap.set("n", "<leader>h", "<cmd>Pick git_hunks<CR>", { desc = "Pick Hunks" })
+  vim.keymap.set("n", "<leader>c", "<cmd>Pick colorschemes<CR>", { desc = "Pick Colorschemes" })
+  vim.keymap.set("n", "<leader>r", "<cmd>Pick resume<CR>", { desc = "Pick Resume" })
+  vim.keymap.set("n", "<leader>ss", "<cmd>Pick lsp scope='document_symbol'<CR>", { desc = "Pick Symbols" })
+  vim.keymap.set("n", "<leader>sd", "<cmd>Pick diagnostic scope='current'<CR>", { desc = "Pick Diagnostics" })
+  vim.keymap.set("n", "<leader>sD", "<cmd>Pick diagnostic scope='all'<CR>", { desc = "Pick Diagnostics All" })
+  vim.keymap.set(
+    "n",
+    "<leader>n",
+    function() require("mini.pick").builtin.files({}, { source = { cwd = vim.fn.stdpath("config") } }) end,
+    { desc = "Pick Neovim" }
+  )
+end)
--- nvim/.config/nvim/plugin/50_plugins.lua
+++ nvim/.config/nvim/plugin/50_plugins.lua
@@ -1,10 +1,11 @@
 local add = vim.pack.add
 local now, now_if_args, later = Config.now, Config.now_if_args, Config.later
 
-vim.opt.runtimepath:prepend("~/odin/koda.nvim")
+-- now(function() add({ "https://github.com/oskarnurm/koda.nvim" }) end)
 -- Benchmark with `=MiniMisc.stat_summary(MiniMisc.bench_time(vim.cmd, 1000, 'colorscheme koda'))`
+vim.opt.runtimepath:prepend("~/odin/koda.nvim")
 require("koda").setup({
-  theme = { dark = "moss", light = "glade" },
+  theme = { dark = "moss", light = "light" },
   styles = { functions = { bold = false } },
 
   -- on_highlights = function(hl, _)
@@ -13,18 +14,14 @@ require("koda").setup({
   --   end
   -- end,
 })
+vim.keymap.set("n", "<leader>k", "<cmd>KodaFetch<CR>")
 vim.cmd.colorscheme("koda")
-
-now(function()
-  require("mini.icons").setup({})
-  later(MiniIcons.tweak_lsp_kind) -- Add LSP kind icons. Useful for 'mini.completion'.
-end)
 
 now_if_args(function()
   add({ "https://github.com/stevearc/oil.nvim" })
   require("oil").setup({
     view_options = { show_hidden = true },
-    skip_confirm_for_simple_edits = false,
+    skip_confirm_for_simple_edits = true,
   })
   vim.keymap.set("n", "<leader>o", "<cmd>Oil<CR>")
 end)
@@ -33,27 +30,6 @@ now_if_args(function()
   add({ { src = "https://github.com/saghen/blink.cmp.git", version = vim.version.range("*") } })
   require("blink.cmp").setup()
 end)
-
--- now_if_args(function()
---   -- Don't show 'Text' suggestios and prioritize snippets
---   local opts = { kind_priority = { Snippet = 99 } }
---   local process_items = function(items, base) return MiniCompletion.default_process_items(items, base, opts) end
---   require("mini.completion").setup({
---     lsp_completion = {
---       source_func = "omnifunc",
---       auto_setup = false, -- enable it lazily below
---       process_items = process_items,
---     },
---   })
---
---   -- Set 'omnifunc' for LSP completion only when needed.
---   local on_attach = function(ev) vim.bo[ev.buf].omnifunc = "v:lua.MiniCompletion.completefunc_lsp" end
---   Config.autocmd("LspAttach", nil, on_attach, "Set 'omnifunc'")
---
---   -- Advertise to servers that Neovim now supports certain set of completion and
---   -- signature features through 'mini.completion'.
---   vim.lsp.config("*", { capabilities = MiniCompletion.get_lsp_capabilities() })
--- end)
 
 now_if_args(function()
   local ts_update = function() vim.cmd("TSUpdate") end
@@ -65,9 +41,6 @@ now_if_args(function()
   })
 
   local languages = {
-    -- To see available languages: `:=require('nvim-treesitter').get_available()`
-    -- - Visit 'SUPPORTED_LANGUAGES.md' file at
-    --   https://github.com/nvim-treesitter/nvim-treesitter/blob/main
     "bash",
     "c",
     "cpp",
@@ -125,129 +98,36 @@ now_if_args(function()
   require("mason").setup()
 end)
 
-later(function() require("mini.extra").setup() end)
-
--- Example usage:
--- - `ci)` - *c*hange *i*inside parenthesis (`)`)
--- - `di(` - *d*elete *i*inside padded parenthesis (`(`)
--- - `yaq` - *y*ank *a*round *q*uote (any of "", '', or ``)
--- - `vif` - *v*isually select *i*inside *f*unction call
--- - `cina` - *c*hange *i*nside *n*ext *a*rgument
-later(function()
-  local ai = require("mini.ai")
-  ai.setup({
-    custom_textobjects = {
-      f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
-      c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
-      o = ai.gen_spec.treesitter({
-        a = { "@conditional.outer", "@loop.outer" },
-        i = { "@conditional.inner", "@loop.inner" },
-      }),
-    },
-  })
-end)
-
--- Example usage:
--- - `cii` - *c*hange *i*nside *i*ndent scope
--- - `Vaiai` - *V*isually select *a*round *i*ndent scope and then again
---   reselect *a*round new *i*indent scope
--- - `[i` / `]i` - navigate to scope's top / bottom
--- later(function() require("mini.indentscope").setup() end)
-
-later(function()
-  require("mini.pick").setup()
-  -- vim.keymap.set("n", "<leader>f", "<cmd>Pick files<CR>", { desc = "Pick Files" })
-  vim.keymap.set("n", "<leader>m", "<cmd>Pick help<CR>", { desc = "Pick Manual" })
-  vim.keymap.set("n", "<leader>/", "<cmd>Pick buf_lines<CR>", { desc = "Pick Lines" })
-  vim.keymap.set("n", "<leader>g", "<cmd>Pick grep_live<CR>", { desc = "Pick Grep" })
-  vim.keymap.set("n", "<leader>h", "<cmd>Pick git_hunks<CR>", { desc = "Pick Hunks" })
-  vim.keymap.set("n", "<leader>c", "<cmd>Pick colorschemes<CR>", { desc = "Pick Colorschemes" })
-  vim.keymap.set("n", "<leader>r", "<cmd>Pick resume<CR>", { desc = "Pick Resume" })
-  vim.keymap.set("n", "<leader>ss", "<cmd>Pick lsp scope='document_symbol'<CR>", { desc = "Pick Symbols" })
-  vim.keymap.set("n", "<leader>sd", "<cmd>Pick diagnostic scope='current'<CR>", { desc = "Pick Diagnostics" })
-  vim.keymap.set("n", "<leader>sD", "<cmd>Pick diagnostic scope='all'<CR>", { desc = "Pick Diagnostics All" })
-  vim.keymap.set(
-    "n",
-    "<leader>n",
-    function() require("mini.pick").builtin.files({}, { source = { cwd = vim.fn.stdpath("config") } }) end,
-    { desc = "Pick Neovim" }
-  )
-end)
-
-later(function()
-  add({ "https://github.com/dmtrKovalenko/fff.nvim" })
-  -- Config.on_packchanged(
-  --   "fff.nvim",
-  --   { "install", "update" },
-  --   function() require("fff.download").download_or_build_binary() end,
-  --   "Download binary for fff.nvim on change"
-  -- )
-
-  vim.g.fff = {
-    lazy_sync = true,
-    debug = { enabled = true, show_scores = true },
-  }
-  require("fff").setup({ prompt = "> ", layout = { prompt_position = "top" } })
-
-  vim.keymap.set("n", "<leader>f", function() require("fff").find_files() end, { desc = "Find Files" })
-  vim.keymap.set("n", "<leader>g", function() require("fff").live_grep() end, { desc = "Grep Live" })
-end)
-
--- Example usage (this may feel intimidating at first, but after practice it
--- becomes second nature during text editing):
--- - `saiw)` - *s*urround *a*dd for *i*nside *w*ord parenthesis (`)`)
--- - `sdf`   - *s*urround *d*elete *f*unction call (like `f(var)` -> `var`)
--- - `srb[`  - *s*urround *r*eplace *b*racket (any of [], (), {}) with padded `[`
--- - `sf*`   - *s*urround *f*ind right part of `*` pair (like bold in markdown)
--- - `shf`   - *s*urround *h*ighlight current *f*unction call
--- - `srn{{` - *s*urround *r*eplace *n*ext curly bracket `{` with padded `{`
--- - `sdl'`  - *s*urround *d*elete *l*ast quote pair (`'`)
-later(function() require("mini.surround").setup() end)
-later(function() require("mini.misc").setup() end)
-later(function() require("mini.jump2d").setup() end)
-later(function()
-  require("mini.visits").setup()
-  vim.keymap.set("n", "<leader>v", "<cmd>Pick visit_paths<CR>", { desc = "Pick Visits" })
-end)
-
-later(function()
-  local snippets = require("mini.snippets")
-  -- Load snippets based on current language by reading files from
-  -- "snippets/" subdirectories from 'runtimepath' directories.
-  snippets.setup({ snippets = { snippets.gen_loader.from_lang() } })
-
-  -- Enable snippets
-  -- Empty tabstops are visualized with inline virtual text (“•”/“∎” for regular/final tabstops)
-  -- Jump to next with <C-l>
-  -- Jump back with <C-h>
-  -- Use <C-n> / <C-p> to select next / previous item.
-  -- Stop session with <C-c>
-  MiniSnippets.start_lsp_server()
-
-  -- Stop all sessions on Normal mode exit
-  local make_stop = function()
-    local au_opts = { pattern = "*:n", once = true }
-    au_opts.callback = function()
-      while MiniSnippets.session.get() do
-        MiniSnippets.session.stop()
-      end
-    end
-    vim.api.nvim_create_autocmd("ModeChanged", au_opts)
-  end
-  local opts = { pattern = "MiniSnippetsSessionStart", callback = make_stop }
-  vim.api.nvim_create_autocmd("User", opts)
-end)
-
 later(function() add({ "https://github.com/rafamadriz/friendly-snippets" }) end)
-
-later(function()
-  add({ "https://github.com/folke/which-key.nvim" })
-  require("which-key").setup({ preset = "helix", icons = { mappings = false } })
-end)
+later(function() add({ "https://github.com/nvim-lua/plenary.nvim" }) end)
+later(function() add({ "https://github.com/tpope/vim-fugitive.git" }) end)
 
 later(function()
   add({ "https://github.com/mbbill/undotree" })
   vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<CR>")
+end)
+
+-- later(function()
+--   add({ "https://github.com/dmtrKovalenko/fff.nvim" })
+--   -- Config.on_packchanged(
+--   --   "fff.nvim",
+--   --   { "install", "update" },
+--   --   function() require("fff.download").download_or_build_binary() end,
+--   --   "Download binary for fff.nvim on change"
+--   -- )
+--
+--   vim.g.fff = {
+--     lazy_sync = true,
+--     debug = { enabled = true, show_scores = true },
+--   }
+--   require("fff").setup({ prompt = "> ", layout = { prompt_position = "top" } })
+--   vim.keymap.set("n", "<leader>f", function() require("fff").find_files() end, { desc = "Find Files" })
+--   vim.keymap.set("n", "<leader>g", function() require("fff").live_grep() end, { desc = "Grep Live" })
+-- end)
+
+later(function()
+  add({ "https://github.com/folke/which-key.nvim" })
+  require("which-key").setup({ preset = "helix", icons = { mappings = false } })
 end)
 
 later(function()
@@ -279,6 +159,8 @@ later(function()
       topdelete = { text = "‾" },
       changedelete = { text = "~" },
     },
+  })
+  require("gitsigns").setup({
     on_attach = function(bufnr)
       local gitsigns = require("gitsigns")
 
@@ -323,7 +205,6 @@ later(function()
   })
 end)
 
-later(function() add({ "https://github.com/tpope/vim-fugitive.git" }) end)
 later(function()
   add({ "https://github.com/Vigemus/iron.nvim.git" })
 
@@ -382,5 +263,3 @@ later(function()
     -- },
   })
 end)
-
-later(function() add({ "https://github.com/nvim-lua/plenary.nvim" }) end)
